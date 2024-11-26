0b1c2afe6c11432e4223e2f98c311af63145a3b6
AlessandroADP <alessandro.davila@tecsup.edu.pe>
2024-11-25 19:58:09 -0500

    a

--- config/services.php
+++ config/services.php
@@ -38,7 +38,8 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://test.com:8000/google-auth/callback',
+        'redirect' => 'http://127.0.0.1:8000/google-auth/callback',
 ],
+
 
 ];
