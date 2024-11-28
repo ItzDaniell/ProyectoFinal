fe6237a3d50c617624d03085e4057dd7af0daf54
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2024-11-27 21:23:48 -0500

    b

---
+++ storage/framework/views/3a0e8293eab5e0743244033599747381.php
@@ -0,0 +1,12 @@
+<?php if($errors->any()): ?>
+    <div <?php echo e($attributes); ?>>
+        <div class=""><?php echo e(__('Whoops! Algo anda mal...')); ?></div>
+
+        <ul class="">
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li><?php echo e($error); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+<?php endif; ?>
+<?php /**PATH C:\Users\ItzDaniel\Desktop\PHP\ProyectoFinal\resources\views/components/validation-errors.blade.php ENDPATH**/ ?>
