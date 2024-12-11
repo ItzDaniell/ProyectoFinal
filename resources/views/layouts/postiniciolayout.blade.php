360299770e71f1bc6e9294ba595fb859c66b90ea
AlessandroADP <alessandro.davila@tecsup.edu.pe>
2024-12-10 23:16:41 -0500

    responsive

--- resources/views/layouts/postiniciolayout.blade.php
+++ resources/views/layouts/postiniciolayout.blade.php
@@ -63,84 +63,100 @@
 </head>
 
 <body>
-    <div class="flex flex-col lg:flex-row">
+    <div class="flex flex-col md:flex-row min-h-screen">
         <!-- Sidebar con ancho fijo -->
-        <div class="bg-orange-700 text-white w-64 h-screen p-4">
-            <div class="flex items-center justify-center h-16 border-b border-white-700">
-                <span class="text-2xl font-semibold">DevShare</span>
-            </div>
-            <nav class="space-y-4 mt-4">
-                <a href="{{ route('Home') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="home-outline" class="text-2xl"></ion-icon>
-                    <span>Página Principal</span>
-                </a>
-                <a href="#" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded" id="openModalBusquedaButton">
-                    <ion-icon name="search-outline" class="text-2xl"></ion-icon>
-                    <span>Búsqueda</span>
-                </a>
-                <a href="{{ route('Noticias') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="newspaper-outline" class="text-2xl"></ion-icon>
-                    <span>Noticias</span>
-                </a>
-                <a href="{{ route('Conferencias') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="laptop-outline" class="text-2xl"></ion-icon>
-                    <span>Conferencias</span>
-                </a>
-                <a href="{{ url('/chats') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="paper-plane-outline" class="text-2xl"></ion-icon>
-                    <span>Mensajes</span>
-                </a>
-                <a href="#" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded" id="openModalCrearButton">
-                    <ion-icon name="create-outline" class="text-2xl"></ion-icon>
-                    <span>Crear</span>
-                </a>
-                <div class="border-t border-white-700 mt-1 pt-1"></div>
-                <a href="{{ route('PerfilUsuario') }}"
-                    class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="person-circle-outline" class="text-2xl"></ion-icon>
-                    <span>Perfil</span>
-                </a>
-                @if(auth()->user()->hasRole('Admin'))
-                <a href="{{ route('Administracion') }}"
-                    class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="settings-outline" class="text-2xl"></ion-icon>
-                    <span>Administrar</span>
-                </a>
-                @endif
-                @if(auth()->user()->hasRole('Usuario'))
-                <a href="#" id="openModal"
-                    class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                    <ion-icon name="information-circle-outline" class="text-2xl"></ion-icon>
-                    <span>Informar Problema</span>
-                </a>
-                @endif
-                <!-- Dropdown button -->
-                <div class="relative cursor-pointer">
-                    <a onclick="toggleMenu()" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                        <ion-icon name="menu-outline" class="text-2xl"></ion-icon>
-                        <span>Más Opciones</span>
-                    </a>
-                    <!-- Dropdown menu (posicionado hacia arriba) -->
-                    <div id="dropdownMenu" style="width: 270px;"
-                        class="absolute bottom-full mb-2 bg-zinc-800 border border-zinc-700 p-2 rounded hidden">
-                        <ul class="space-y-2">
-                            <a href="{{ route('Configuracion') }}"
-                                class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                                <ion-icon name="settings-outline" class="text-2xl"></ion-icon>
-                                <span>Configuración</span>
-                            </a>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
-                                    <ion-icon name="log-out-outline" class="text-2xl"></ion-icon>
-                                    <span>Cerrar Sesión</span>
-                                </button>
-                            </form>
-                        </ul>
+        <div id="responsiveDiv">
+            <div
+                class="bg-orange-700 text-white md:w-64 md:h-screen md:relative p-1 fixed bottom-0 left-0 right-0 flex md:flex-col md:justify-start md:items-start md:space-y-2 items-center">
+                <nav class="space-y-4 w-full md:w-auto flex md:flex-col justify-around">
+                    <!-- Encabezado (visible solo en pantallas grandes) -->
+                    <div class="flex items-center justify-center h-16 border-b border-white w-full">
+                        <span class="text-2xl font-semibold">DevShare</span>
                     </div>
-                </div>
-            </nav>
+                    <!-- Elementos de Navegación -->
+                    <a href="{{ route('Home') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="home-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Página Principal</span>
+                    </a>
+                    <a href="#" id="openModalBusquedaButton"
+                        class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="search-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Búsqueda</span>
+                    </a>
+                    <a href="{{ route('Noticias') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="newspaper-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Noticias</span>
+                    </a>
+                    <a href="{{ route('Conferencias') }}"
+                        class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="laptop-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Conferencias</span>
+                    </a>
+                    <a href="{{ url('/chats') }}" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="paper-plane-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Mensajes</span>
+                    </a>
+                    <a href="#" id="openModalCrearButton"
+                        class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="create-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Crear</span>
+                    </a>
+                    <div class="hidden md:block border-t border-white mt-1 pt-1 w-full"></div>
+                    <a href="{{ route('PerfilUsuario') }}"
+                        class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                        <ion-icon name="person-circle-outline" class="text-2xl"></ion-icon>
+                        <span class="block md:inline">Perfil</span>
+                    </a>
+
+                    @if(auth()->user()->hasRole('Admin'))
+                        <a href="{{ route('Administracion') }}"
+                            class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                            <ion-icon name="settings-outline" class="text-2xl"></ion-icon>
+                            <span class="block md:inline">Administrar</span>
+                        </a>
+                    @endif
+
+                    @if(auth()->user()->hasRole('Usuario'))
+                        <a href="#" id="openModal" class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                            <ion-icon name="information-circle-outline" class="text-2xl"></ion-icon>
+                            <span class="block md:inline">Informar Problema</span>
+                        </a>
+                    @endif
+
+                    <!-- Más Opciones -->
+                    <div class="relative">
+                        <a onclick="toggleMenu()"
+                            class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded cursor-pointer">
+                            <ion-icon name="menu-outline" class="text-2xl"></ion-icon>
+                            <span class="block md:inline">Más Opciones</span>
+                        </a>
+                        <div id="dropdownMenu"
+                            class="absolute hidden mt-2 bg-zinc-800 border border-zinc-700 p-2 rounded w-64">
+                            <ul class="space-y-2">
+                                <li>
+                                    <a href="{{ route('Configuracion') }}"
+                                        class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded">
+                                        <ion-icon name="settings-outline" class="text-2xl"></ion-icon>
+                                        <span>Configuración</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button type="submit"
+                                            class="flex items-center space-x-3 hover:bg-zinc-700 p-2 rounded w-full text-left">
+                                            <ion-icon name="log-out-outline" class="text-2xl"></ion-icon>
+                                            <span>Cerrar Sesión</span>
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
+
         <div id="modal" class="fixed inset-0 bg-gray-800 bg-opacity-75 hidden overflow-y-auto z-50">
             <div class="flex items-center justify-center min-h-screen p-4">
                 <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-4xl relative">
@@ -163,15 +179,19 @@
                             <!-- Sección de imagen -->
                             <div class="lg:w-1/3 flex-shrink-0">
                                 <label for="imageInput" class="block text-gray-700 font-medium mb-2">Archivo</label>
-                                <div class="border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center h-96 sm:h-96 lg:h-96">
+                                <div
+                                    class="border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center h-96 sm:h-96 lg:h-96">
                                     <div class="bg-gray-100 p-6 rounded-lg mb-4">
                                         <ion-icon name="image-outline" class="text-6xl text-gray-400"></ion-icon>
                                     </div>
-                                    <input type="file" name="archivo" class="hidden" id="imageInput" accept="image/*,application/pdf,.doc,.docx,.rar,.zip" />
-                                    <button type="button" onclick="document.getElementById('imageInput').click();" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-48">
+                                    <input type="file" name="archivo" class="hidden" id="imageInput"
+                                        accept="image/*,application/pdf,.doc,.docx,.rar,.zip" />
+                                    <button type="button" onclick="document.getElementById('imageInput').click();"
+                                        class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-48">
                                         Seleccionar archivo
                                     </button>
-                                    <p id="fileName" class="text-sm text-gray-500 mt-2 text-center truncate max-w-full overflow-hidden text-ellipsis">
+                                    <p id="fileName"
+                                        class="text-sm text-gray-500 mt-2 text-center truncate max-w-full overflow-hidden text-ellipsis">
                                         Ningún archivo seleccionado
                                     </p>
                                 </div>
@@ -211,7 +231,8 @@
                 </div>
             </div>
         </div>
-        <div id="searchModal" class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center hidden z-50">
+        <div id="searchModal"
+            class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center hidden z-50">
             <div class="bg-white w-96 p-6 rounded shadow-lg">
                 <div class="flex justify-between items-center border-b pb-2 mb-4">
                     <h2 class="text-xl font-bold">Buscar Personas</h2>
@@ -233,15 +254,18 @@
             <div class="flex items-center justify-center min-h-screen">
                 <div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-lg">
                     <h2 class="text-lg font-semibold mb-4 ">Informar Problema</h2>
-                    <form id="problemForm" method="POST" enctype="multipart/form-data" action="{{ route('informes.store') }}">
+                    <form id="problemForm" method="POST" enctype="multipart/form-data"
+                        action="{{ route('informes.store') }}">
                         @csrf
                         <div class="mb-4">
-                            <label for="tipo" class="block text-sm font-medium text-gray-700">Tipo de problema que presenta</label>
+                            <label for="tipo" class="block text-sm font-medium text-gray-700">Tipo de problema que
+                                presenta</label>
                             <select type="text" name="tipo" id="tipo" class="w-full border rounded-md p-2" required>
                                 <option value="">Seleccione el problema</option>
                                 <option value="Errores de Carga Lenta">Errores de carga lenta</option>
                                 <option value="Enlaces Rotos">Enlaces rotos</option>
-                                <option value="Problemas de Compatibilidad entre Navegadores">Problemas de compatibilidad entre navegadores</option>
+                                <option value="Problemas de Compatibilidad entre Navegadores">Problemas de
+                                    compatibilidad entre navegadores</option>
                                 <option value="Errores en Formularios">Errores en formularios</option>
                                 <option value="Problemas de Seguridad">Problemas de seguridad</option>
                                 <option value="Falta de Accesibilidad">Falta de accesibilidad</option>
@@ -249,15 +273,18 @@
                             </select>
                         </div>
                         <div class="mb-4">
-                            <label for="imagen" class="block text-sm font-medium text-gray-700">Subir Imagen (opcional)</label>
+                            <label for="imagen" class="block text-sm font-medium text-gray-700">Subir Imagen
+                                (opcional)</label>
                             <input type="file" name="imagen" id="imagen" class="w-full border rounded-md p-2">
                         </div>
                         <div class="mb-4">
                             <label for="descripcion" class="block text-sm font-medium text-gray-700">Descripción</label>
-                            <textarea name="descripcion" id="descripcion" class="w-full border rounded-md p-2" rows="4" maxlength="2048" required></textarea>
+                            <textarea name="descripcion" id="descripcion" class="w-full border rounded-md p-2" rows="4"
+                                maxlength="2048" required></textarea>
                         </div>
                         <div class="flex justify-end">
-                            <button type="button" id="cancelProblem" class="bg-gray-300 text-gray-700 px-4 py-2 rounded-md mr-2">Cancelar</button>
+                            <button type="button" id="cancelProblem"
+                                class="bg-gray-300 text-gray-700 px-4 py-2 rounded-md mr-2">Cancelar</button>
                             <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">Enviar</button>
                         </div>
                     </form>
@@ -319,7 +346,46 @@
                 });
             });
         </script>
-        <div class="flex-1 bg-gray-100 p-4 overflow-y-auto h-screen">
+        <script>
+            function handleResponsiveChanges() {
+                // Actualizar visibilidad del div
+                const headerDiv = document.querySelector('.flex.items-center.justify-center.h-16.border-b.border-white.w-full');
+                if (window.innerWidth < 768) {
+                    headerDiv.style.display = 'none'; // Ocultar el div
+                } else {
+                    headerDiv.style.display = 'flex'; // Mostrar el div
+                }
+
+                // Actualizar clases del div responsivo
+                const responsiveDiv = document.getElementById('responsiveDiv');
+                if (responsiveDiv) {
+                    if (window.innerWidth < 768) {
+                        responsiveDiv.className = '';
+                    } else {
+                        responsiveDiv.className = 'w-64';
+                    }
+                }
+
+                // Actualizar visibilidad de los textos
+                const elements = document.querySelectorAll('span');
+                elements.forEach(element => {
+                    if (window.innerWidth <= 768) {
+                        if (element.classList.contains('block') && element.classList.contains('md:inline')) {
+                            element.classList.remove('block');
+                            element.classList.add('hidden');
+                        }
+                    } else {
+                        if (element.classList.contains('hidden') && element.classList.contains('md:inline')) {
+                            element.classList.remove('hidden');
+                            element.classList.add('block');
+                        }
+                    }
+                });
+            }
+            handleResponsiveChanges(); // Llamada inicial para establecer el estado
+            window.addEventListener('resize', handleResponsiveChanges); // Evento al cambiar el tamaño de la ventana
+        </script>
+        <div class="flex-1 bg-white p-4 overflow-y-auto h-screen">
             @yield('content')
         </div>
     </div>
