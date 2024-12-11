dbd8e0b1eb8bb359f8cf8b06c2c8476dfbaf1477
AlessandroADP <alessandro.davila@tecsup.edu.pe>
2024-12-10 23:31:22 -0500

    cambios minimos

--- resources/views/layouts/postiniciolayout.blade.php
+++ resources/views/layouts/postiniciolayout.blade.php
@@ -178,15 +178,6 @@
                         <div class="flex flex-col lg:flex-row space-y-6 lg:space-y-0 lg:space-x-6 mt-6">
                             <!-- Sección de imagen -->
                             <div class="lg:w-1/3 flex-shrink-0">
-<<<<<<< HEAD
-                                <label for="imageInput" class="block text-gray-700 font-medium mb-2">Archivo (Obligotorio)</label>
-                                <div class="border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center h-96 sm:h-96 lg:h-96">
-                                    <div class="bg-gray-100 p-6 rounded-lg mb-4">
-                                        <ion-icon name="image-outline" class="text-6xl text-gray-400"></ion-icon>
-                                    </div>
-                                    <input type="file" name="archivo" class="hidden" id="imageInput" required accept="image/*,application/pdf,.doc,.docx,.rar,.zip" />
-                                    <button type="button" onclick="document.getElementById('imageInput').click();" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-48">
-=======
                                 <label for="imageInput" class="block text-gray-700 font-medium mb-2">Archivo</label>
                                 <div
                                     class="border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center h-96 sm:h-96 lg:h-96">
@@ -197,7 +188,6 @@
                                         accept="image/*,application/pdf,.doc,.docx,.rar,.zip" />
                                     <button type="button" onclick="document.getElementById('imageInput').click();"
                                         class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 w-48">
->>>>>>> 360299770e71f1bc6e9294ba595fb859c66b90ea
                                         Seleccionar archivo
                                     </button>
                                     <p id="fileName"
